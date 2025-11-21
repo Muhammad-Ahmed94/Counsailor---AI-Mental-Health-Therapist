eaf91e209ac33f2cf95d6c71a1cf6d5830f27e8c
Muhammad-Ahmed94 <muneebbum757@gmail.com>
2025-11-21 12:07:09 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+    <div className='text-2xl text-gray-500 '>COUNSAILOR</div>
+    <div className='text-5xl text-red-600'>AI POWERED MENTAL ASSINSSTANCE</div>
+    </>
+  )
+}
+
+export default App
